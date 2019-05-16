e10695a834130a9c7dd42c76277c4acbd07dcc9a
Bahman Movaqar <bahman@bahmanm.com>
2019-05-16 11:12:23 +0300

    Only write the address to file
    Instead of the variable definition form, simply dump the address
    to the file.

--- util/schedpit_addr.c
+++ util/schedpit_addr.c
@@ -1,19 +1,19 @@
 /******************************************************************************
- * IFEM OS - util/schedpit_addr.c                                             * 
+ * IFEM OS - util/schedpit_addr.c                                             *
  * Copyright (C) 2008 Bahman Movaqar (bahman AT bahmanm.com)                  *
  *                                                                            *
- * This program is free software; you can redistribute it and/or              * 
- * modify it under the terms of the GNU General Public License                * 
- * as published by the Free Software Foundation; either version 2             * 
- * of the License, or (at your option) any later version.                     * 
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
  *                                                                            *
  * This program is distributed in the hope that it will be useful,            *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
- * GNU General Public License for more details.                               * 
- *                                                                            * 
- * You should have received a copy of the GNU General Public License          * 
- * along with this program; if not, write to the Free Software                * 
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software                *
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,                 *
  * MA  02110-1301, USA.                                                       *
  ******************************************************************************/
@@ -40,7 +40,7 @@
 int main(int argc, char *argv[])
 {
 	int fd;
-	long addr, kernel_size;
+	unsigned long addr, kernel_size;
 	char str[30];
 
 	if (argc == 1) {
@@ -49,21 +49,21 @@ int main(int argc, char *argv[])
 	}
 
 	if ((fd = open(argv[1], O_RDWR)) < 0) err_exit(1);
-	
+
 	if ((kernel_size = lseek(fd,0,SEEK_END)) < 0) err_exit(1);
 	addr = kernel_size + 0x600;
 	close(fd);
-	
-	sprintf(str, "SCHEDPIT_ADDR=\t0x%X\n#", addr, '\0');
-	if ((fd = open(argv[2], O_WRONLY | O_CREAT)) < 0) err_exit(1); 
+
+	sprintf(str, "0x%lx", addr);
+	if ((fd = open(argv[2], O_WRONLY | O_CREAT, 0640)) < 0) err_exit(1);
 	if (write(fd, str, strlen(str)) <= 0) err_exit(1);
 	close(fd);
-	
+
 	fprintf(stderr, "=====================\n");
 	fprintf(stderr, "schedpit_addr Report:\n");
-	fprintf(stderr, "=====================\n"); 
+	fprintf(stderr, "=====================\n");
 	fprintf(stderr, "ifem-os.bin size = %ld bytes\n", kernel_size);
 	fprintf(stderr, "Kernel image successfully modified!\n");
-	
+
 	return 0;
 }
